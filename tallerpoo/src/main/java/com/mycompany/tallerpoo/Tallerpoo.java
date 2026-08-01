0c5cc4658ea2ce3c5d17c0d71009726a95b934f3
JesusDavid1405 <jesusdavidfierrorivera817@gmail.com>
2026-07-31 19:45:42 -0500

    se añadio un nuevo proyecto

---
+++ tallerpoo/src/main/java/com/mycompany/tallerpoo/Tallerpoo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tallerpoo;
+
+/**
+ *
+ * @author APRENDIZ
+ */
+public class Tallerpoo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
